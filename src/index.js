971a1ef00d272bff20c18337219b74d319ecb2b3
Aksultan Aitkozhin <52781023+AksultanAitkozha@users.noreply.github.com>
2021-03-09 15:35:47 +0600

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!arr || arr.length == 0) {
+        return 0;
+    }
+    var len = arr.length, min = Infinity;
+    while (len--) {
+        if (arr[len] < min) {
+            min = arr[len];
+        }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arr || arr.length == 0) {
+    return 0;
+  }
+  var len = arr.length, max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arr || arr.length == 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += Number(arr[i]);
+    }
+    return sum / arr.length;
 }
